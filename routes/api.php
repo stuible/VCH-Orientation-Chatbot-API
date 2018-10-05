5fce84f151895117dfcf64a6608a6be6b6d33f4f
Josh Stuible <shrewdlove@gmail.com>
2018-10-05 13:15:54 -0700

    Setup CRUD routes for intents

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Http\Request;
+Use App\Intent;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,8 +18,45 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-//Return Slot response
-Route::get('{intent}/{slot}', 'SlotController@show');
+// INTENTS
 
-//Get Article by ID
-// Route::get('intents', 'IntentController@show');
+//All Intents
+Route::get('intents', 'IntentController@index');
+
+//Single Intent
+Route::get('intents/{name}', 'IntentController@show');
+
+//Create Intent
+Route::post('intents', 'IntentController@store');
+
+//Create Intent
+Route::put('intents/{name}', 'IntentController@store');
+
+//Destroy Intent
+Route::delete('intents/{name}', 'IntentController@destroy');
+
+//All slots by intent
+// Route::get('{intent}', 'IntentController@select');
+
+
+// SLOTS
+
+//Single slot routes
+Route::get('intents/{intent}/slots/{slot}', 'SlotController@show');
+
+// Route::post('/', function(Request $request) {
+//     return Intent::create($request->all);
+// });
+
+// Route::put('/{id}', function(Request $request, $id) {
+//     $article = Article::findOrFail($id);
+//     $article->update($request->all());
+
+//     return $article;
+// });
+
+// Route::delete('/{id}', function($id) {
+//     Intent::find($id)->delete();
+
+//     return 204;
+// });
