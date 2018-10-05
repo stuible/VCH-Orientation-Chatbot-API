c30b99f007d26058ee04ed7c090a97f8608d50c6
Josh Stuible <shrewdlove@gmail.com>
2018-10-05 13:16:54 -0700

    Installed Vue and Bulma

---
+++ resources/js/components/Intents.vue
@@ -0,0 +1,57 @@
+<template>
+    <div id="intents">
+        <h1>Intents</h1>
+        <div class="card" v-for="intent in intents" v-bind:key="intent.id">
+            <header class="card-header">
+                <p class="card-header-title">
+                {{ intent.name }}
+                </p>
+                <!-- <a href="#" class="card-header-icon" aria-label="more options">
+                <span class="icon">
+                    <i class="fas fa-angle-down" aria-hidden="true"></i>
+                </span>
+                </a> -->
+            </header>
+            <div class="card-content">
+                <div class="content">
+                {{ intent.description }}
+                <br>
+                <time datetime="2016-1-1">{{ intent.created_at }}</time>
+                </div>
+            </div>
+            <!-- <footer class="card-footer">
+                <a href="#" class="card-footer-item">Save</a>
+                <a href="#" class="card-footer-item">Edit</a>
+                <a href="#" class="card-footer-item">Delete</a>
+            </footer> -->
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                intents: [],
+                intent: {
+                    id: '',
+                    title: '',
+                    description: ''
+                }
+            }
+        },
+        created() {
+            this.fetchIntents();
+        },
+
+        methods: {
+            fetchIntents() {
+                fetch('api/intents/')
+                    .then(res => res.json())
+                    .then(res => {
+                        this.intents = res.data;
+                    })
+            }
+        }
+    }
+</script>
