734ad856ecb0018f003141425490c29affee673a
Josh Stuible <shrewdlove@gmail.com>
2018-10-11 16:53:47 -0700

    Created Slot resource and API logic to display list of slots belonging to an Intent (WIP)

--- app/Http/Controllers/SlotController.php
+++ app/Http/Controllers/SlotController.php
@@ -4,18 +4,32 @@ namespace App\Http\Controllers;
 use Illuminate\Support\Facades\DB;
 
 use Illuminate\Http\Request;
+use App\Slot;
+use App\Http\Resources\Slot as SlotResource;
 
 class SlotController extends Controller
 {
-    public function show($intent, $slot)
+    public function index($intent)
     {
-        $response = DB::table('intents')
-            ->join('slots', 'intents.id', '=', 'slots.intentID')
-            ->where('intents.name', '=', $intent)
-            ->where('slots.title', '=', $slot)
-            ->select('response')
-            ->get();
+        // Get all Intents
+        // $slots = new Slot;
+        // echo "<p class='lmao'> $slots </p>";
+        $slots = Slot::paginate(5000);
 
-        return $response;
+        $slots = $slots->where('intentID', 1);
+
+        return SlotResource::collection($slots);
     }
+
+    // public function show($intent, $slot)
+    // {
+    //     $response = DB::table('intents')
+    //         ->join('slots', 'intents.id', '=', 'slots.intentID')
+    //         ->where('intents.name', '=', $intent)
+    //         ->where('slots.title', '=', $slot)
+    //         ->select('response')
+    //         ->get();
+
+    //     return $response;
+    // }
 }
