10f395fb977137de8f0b581077c688ef007dd9ef
Josh Stuible <shrewdlove@gmail.com>
2018-10-12 13:55:07 -0700

    Tested method for routing all URLs to Vue (Didn't work)

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,7 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Route::get('{slug}', function() {
+//     return view('welcome');
+//  })->where('slug', '(?!api)([A-z\d-\/_.]+)?');
