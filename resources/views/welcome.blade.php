b426e919f1b7d3edf6cd7dee2d6f668eaeff0b8d
Josh Stuible <shrewdlove@gmail.com>
2018-10-12 13:55:27 -0700

    Switched font and moved things into their own componenets

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -11,16 +11,22 @@
         <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 
         <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet" type="text/css">
+        <link href='https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Material+Icons' rel="stylesheet">
 
     </head>
     <body>
+        
         <div id="app">
-            <navbar></navbar>
-            {{-- <breadcrumbs></breadcrumbs> --}}
-            <div class="container">
-                <router-view></router-view>
-            </div>
+                <v-app>
+                    <navigation></navigation>
+                        
+                        <v-content>
+                          <v-container fluid>
+                            <router-view></router-view>
+                          </v-container>
+                        </v-content>
+                        <v-footer app></v-footer>
+                </v-app>
         </div>
 
         <script src="{{ asset('js/app.js') }}"></script>
